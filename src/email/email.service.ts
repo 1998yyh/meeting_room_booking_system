8cb103ab0467de3f2d621657f5c688ddd371cdec
hao.yang <hao.yang@haoyangdeMacBook-Pro.local>
2024-08-14 19:14:11 +0800

    register

---
+++ src/email/email.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { createTransport, Transporter } from 'nodemailer';
+
+@Injectable()
+export class EmailService {
+  transporter: Transporter;
+
+  constructor() {
+    this.transporter = createTransport({
+      host: 'smtp.qq.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: '532438158@qq.com',
+        pass: 'dtkhkxmyekthbhce',
+      },
+    });
+  }
+
+  async sendMail({ to, subject, html }) {
+    await this.transporter.sendMail({
+      from: {
+        name: '会议室预定系统',
+        address: '532438158@qq.com',
+      },
+      to,
+      subject,
+      html,
+    });
+  }
+}
