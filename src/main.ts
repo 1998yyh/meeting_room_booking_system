77c1f68b9fd2362b879ce4d20c03d8eae3175665
hao yang <41160134+1998yyh@users.noreply.github.com>
2024-08-20 14:31:09 +0800

    login

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   const configservice = app.get(ConfigService);
   await app.listen(configservice.get('nest_server_port'));
