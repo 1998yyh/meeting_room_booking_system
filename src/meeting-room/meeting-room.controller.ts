b5ead72d60e15bdf48aeda5046d74147f9977f45
hao yang <41160134+1998yyh@users.noreply.github.com>
2024-09-19 16:48:16 +0800

    meeting room

--- src/meeting-room/meeting-room.controller.ts
+++ src/meeting-room/meeting-room.controller.ts
@@ -2,21 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query, DefaultValueP
 import { MeetingRoomService } from './meeting-room.service';
 import { generateParseIntPipe } from 'src/utils';
 import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
+import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
 
 @Controller('meeting-room')
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) {}
 
+  @Get(':id')
+  async find(@Param('id') id: number) {
+    return await this.meetingRoomService.findById(id);
+  }
+
   @Get('list')
   async list(
     @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo')) pageNo: number,
     @Query('pageSize', new DefaultValuePipe(2), generateParseIntPipe('pageSize')) pageSize: number,
+    @Query('name') name: string,
+    @Query('capacity') capacity: number,
+    @Query('equipment') equipment: string,
   ) {
-    return await this.meetingRoomService.find(pageNo, pageSize);
+    return await this.meetingRoomService.find(pageNo, pageSize, name, capacity, equipment);
   }
 
   @Post('create')
   async create(@Body() meetingRoomDto: CreateMeetingRoomDto) {
     return await this.meetingRoomService.create(meetingRoomDto);
+  }
+
+  @Post('update')
+  async update(@Body() meetingRoomDto: UpdateMeetingRoomDto) {
+    return await this.meetingRoomService.update(meetingRoomDto);
+  }
+
+  @Post(':id')
+  async delete(@Param('id') id: number) {
+    return await this.meetingRoomService.delete(id);
   }
 }
--- src/meeting-room/meeting-room.service.ts
+++ src/meeting-room/meeting-room.service.ts
@@ -1,25 +1,42 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
 import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { MeetingRoom } from './entities/meeting-room.entity';
-import { Repository } from 'typeorm';
+import { EntityManager, Like, Repository } from 'typeorm';
 
 @Injectable()
 export class MeetingRoomService {
   @InjectRepository(MeetingRoom)
   private repository: Repository<MeetingRoom>;
 
-  async find(pageNo: number, pageSize: number) {
+  @InjectEntityManager()
+  entityManager: EntityManager;
+
+  async find(pageNo: number, pageSize: number, name: string, capacity: number, equipment: string) {
     if (pageNo < 1) {
-      throw new BadRequestException('页面最小为1');
+      throw new BadRequestException('页码最小为 1');
+    }
+    const skipCount = (pageNo - 1) * pageSize;
+
+    const condition: Record<string, any> = {};
+
+    if (name) {
+      condition.name = Like(`%${name}%`);
     }
 
-    const skip = (pageNo - 1) * pageSize;
+    if (equipment) {
+      condition.equipment = Like(`%${equipment}%`);
+    }
+
+    if (capacity) {
+      condition.capacity = capacity;
+    }
 
     const [meetingRooms, totalCount] = await this.repository.findAndCount({
-      skip,
+      skip: skipCount,
       take: pageSize,
+      where: condition,
     });
 
     return {
@@ -28,11 +45,66 @@ export class MeetingRoomService {
     };
   }
 
+  async findById(id: number) {
+    return await this.repository.findOneBy({
+      id,
+    });
+  }
+
+  async delete(id: number) {
+    // const bookings = await this.entityManager.findBy(Booking, {
+    //   room: {
+    //     id: id,
+    //   },
+    // });
+
+    // for (let i = 0; i < bookings.length; i++) {
+    //   this.entityManager.delete(Booking, bookings[i].id);
+    // }
+    await this.repository.delete(id);
+    return 'success';
+  }
+
   async create(meetingRoomDto: CreateMeetingRoomDto) {
-    console.log(meetingRoomDto);
+    const room = await this.repository.findOneBy({
+      name: meetingRoomDto.name,
+    });
 
-    const meetingRoom = new MeetingRoom();
+    if (room) {
+      throw new BadRequestException('会议室名字已存在');
+    }
 
-    // meetingRoom.createTime = Date.now();
+    return await this.repository.save(meetingRoomDto);
+  }
+
+  async update(meetingRoomDto: UpdateMeetingRoomDto) {
+    const meetingRoom = await this.repository.findOneBy({
+      id: meetingRoomDto.id,
+    });
+
+    if (!meetingRoom) {
+      throw new BadRequestException('会议室不存在');
+    }
+
+    meetingRoom.capacity = meetingRoomDto.capacity;
+    meetingRoom.location = meetingRoomDto.location;
+    meetingRoom.name = meetingRoomDto.name;
+
+    if (meetingRoomDto.description) {
+      meetingRoom.description = meetingRoomDto.description;
+    }
+
+    if (meetingRoomDto.equipment) {
+      meetingRoom.equipment = meetingRoomDto.equipment;
+    }
+
+    await this.repository.update(
+      {
+        id: meetingRoom.id,
+      },
+      meetingRoom,
+    );
+
+    return 'success';
   }
 }
